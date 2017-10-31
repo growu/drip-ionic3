8b9f29fe31ce977f688f1bdfcf47d603cb4d110b
Jason.z <ccnuzxg@gmail.com>
2017-10-31 12:54:18 +0800

    修复日历数据加载延时问题

--- src/pages/goal-detail-calendar/goal-detail-calendar.ts
+++ src/pages/goal-detail-calendar/goal-detail-calendar.ts
@@ -1,83 +1,89 @@
-import { Component } from '@angular/core';
-import { App, IonicPage, NavController, NavParams } from 'ionic-angular';
-import { CalendarComponentOptions, DayConfig } from 'ion2-calendar'
+import {Component} from '@angular/core';
+import {App, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {CalendarComponentOptions, DayConfig} from 'ion2-calendar'
 import * as moment from 'moment'
 
-import { UserProvider } from '../../providers/user/user'
+import {UserProvider} from '../../providers/user/user'
 
 @IonicPage({
-  name: 'goal-calendar',
-  segment: 'goal/:id/calendar'
+    name: 'goal-calendar',
+    segment: 'goal/:id/calendar'
 })
 @Component({
-  selector: 'page-goal-detail-calendar',
-  templateUrl: 'goal-detail-calendar.html',
+    selector: 'page-goal-detail-calendar',
+    templateUrl: 'goal-detail-calendar.html',
 })
 export class GoalDetailCalendarPage {
 
-  _daysConfig: DayConfig[] = [];
-  event:any = {};
-  selectDay:any;
+    event: any = {};
+    selectDay: any;
 
-  calendarOptions: CalendarComponentOptions = {
-    from:new Date(2009,1,1),
-    to:new Date(),
-    pickMode: 'single',
-    daysConfig: this._daysConfig,
-    monthFormat: 'yyyy 年 MM 月 ',
-    weekdays: ['天', '一', '二', '三', '四', '五', '六'],
-  };
+    calendarOptions: CalendarComponentOptions = {
+    };
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private userProvider: UserProvider,
-              private app: App) {
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private userProvider: UserProvider,
+                private app: App) {
+    }
 
-  ionViewDidLoad() {
-    this.getGoalCalendar();
-    this.selectDay = moment().format("YYYY-MM-DD");
-    this.getGoalDay();
-  }
+    ionViewDidEnter() {
+        this.getGoalCalendar();
+        this.selectDay = moment().format("YYYY-MM-DD");
+        this.getGoalDay();
+    }
 
-  getGoalCalendar() {
-    this.userProvider.getGoalCalendar(this.navParams.get('id')).then((data)=>{
-      data.forEach((item,index)=>{
-        let cssClass:string = "between";
-        if(index == 0 ) cssClass = "start";
-        if(index == data.length-1 ) cssClass = "end";
+    getGoalCalendar() {
+        this.userProvider.getGoalCalendar(this.navParams.get('id')).then((data) => {
+            let days:DayConfig[] = [];
 
-        this._daysConfig.push({
-          date: new Date(item.checkin_day),
-          cssClass: cssClass
-        })
-      });
-    });
-  }
+            data.forEach((item, index) => {
+                let cssClass: string = "between";
+                if (index == 0) cssClass = "start";
+                if (index == data.length - 1) cssClass = "end";
 
-  getGoalDay() {
-    this.userProvider.getGoalDay(this.navParams.get('id'),this.selectDay).then((data)=>{
-      if(Object.keys(data).length === 0) {
-        this.event = {};
-      } else {
-        this.event = data;
-      }
-      console.log(!this.event);
-    });
-  }
+                days.push({
+                    date: new Date(item.checkin_day),
+                    cssClass: cssClass
+                })
+            });
 
-  onChange($event) {
-    this.selectDay = $event;
-    this.getGoalDay();
-  }
+            setTimeout(() => {
+                this.calendarOptions = {
+                    from: new Date(2009, 1, 1),
+                    to: new Date(),
+                    pickMode: 'single',
+                    daysConfig: days,
+                    monthFormat: 'yyyy 年 MM 月 ',
+                    weekdays: ['天', '一', '二', '三', '四', '五', '六'],
+                }
+            },300);
+        });
+    }
 
-  goEventDetailPage(id) {
-    this.app.getRootNav().push('event-detail',{id:id});
-  }
+    getGoalDay() {
+        this.userProvider.getGoalDay(this.navParams.get('id'), this.selectDay).then((data) => {
+            if (Object.keys(data).length === 0) {
+                this.event = {};
+            } else {
+                this.event = data;
+            }
+            console.log(!this.event);
+        });
+    }
 
-  goGoalCheckinPage() {
-    let id = this.navParams.get('id');
-    this.app.getRootNav().push('goal-checkin',{id:id,day:this.selectDay});
-  }
+    onChange($event) {
+        this.selectDay = $event;
+        this.getGoalDay();
+    }
+
+    goEventDetailPage(id) {
+        this.app.getRootNav().push('event-detail', {id: id});
+    }
+
+    goGoalCheckinPage() {
+        let id = this.navParams.get('id');
+        this.app.getRootNav().push('goal-checkin', {id: id, day: this.selectDay});
+    }
 
 }
